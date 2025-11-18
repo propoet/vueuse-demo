0ce1ad3af289bd24235faca6a23ec531b317954e
propoet <propoet@163.com>
2025-11-18 08:12:06 +0800

    createInjectionState

---
+++ src/components/02_createInjectionState/demo/useCounterStore.ts
@@ -0,0 +1,23 @@
+import { ref, computed } from "vue";
+import { createInjectionState } from "@vueuse/core";
+
+const [useProvideCounterStore, useCounterStore] = createInjectionState(
+  (initialValue: number) => {
+    // state
+    const count = ref(initialValue);
+    // getters
+    const double = computed(() => count.value * 2);
+
+    // actions
+    function increment() {
+      count.value++;
+    }
+    return {
+      count,
+      double,
+      increment,
+    };
+  }
+);
+export { useProvideCounterStore };
+export { useCounterStore };
