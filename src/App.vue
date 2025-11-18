7f20f3bab63ec2abfc46cd7427d0419e043b92e7
propoet <propoet@163.com>
2025-11-18 19:45:04 +0800

    provideAndInjectLocal

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import createGlobalState from "./components/01_createGlobalState/index.vue";
 import createInjectionState from "./components/02_createInjectionState/index.vue";
 import createSharedComposable from "./components/03_createSharedComposable/index.vue";
+import provideAndInjectLocal from "./components/04_provideAndInjectLocal/index.vue";
 </script>
 
 <template>
-  <createGlobalState />
-  <createInjectionState />
+  <provideAndInjectLocal />
   <createSharedComposable />
+  <createInjectionState />
+  <createGlobalState />
 </template>
 
 <style scoped></style>
---
+++ src/components/04_provideAndInjectLocal/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>04_provideAndInjectLocal 提供和注入局部状态</h1>
+  {{ name }}
+  <hr />
+</template>
+
+<script setup lang="ts">
+import { provideLocal, injectLocal } from "@vueuse/core";
+
+provideLocal("name", "John");
+const name = injectLocal("name");
+</script>
